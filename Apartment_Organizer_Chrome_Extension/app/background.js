2e6e402c3d32bb0d440e1ad1c9bea9c6ae199f95
Andrew Davidson <davids.andrewc@gmail.com>
2018-03-17 00:56:26 -0700

    Removed previous BrowserAction listener

--- Apartment_Organizer_Chrome_Extension/app/background.js
+++ Apartment_Organizer_Chrome_Extension/app/background.js
@@ -32,18 +32,3 @@ function addBrowserActionListener() {
 }
 
 addBrowserActionListener();
-
-/*
-chrome.runtime.onInstalled.addListener(addBrowserActionListener);
-
-if (chrome.runtime && chrome.runtime.onStartup) {
-    chrome.runtime.onStartup.addListener(addBrowserActionListener);
-} else {
-    // This hack is needed because Chrome 22 does not persist browserAction icon
-    // state, and also doesn't expose onStartup. So the icon always starts out in
-    // wrong state. We don't actually use onStartup except as a clue that we're
-    // in a version of Chrome that has this problem.
-    chrome.windows.onCreated.addListener(addBrowserActionListener);
-}
-
-*/
